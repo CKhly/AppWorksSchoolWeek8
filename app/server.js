62a17feaf2d8d96eb5bc7b5f34f1c6934fd804b8
Lee Hung <lee920024@gmail.com>
2022-06-04 23:22:06 +0800

    feat: add server with express

--- app/server.js
+++ app/server.js
@@ -1,10 +1,47 @@
 const express = require("express");
 const app = express();
+const port = 3000
+
 const axios = require("axios")
-const mongoose = require('mongoose');
 const { connectDB } = require('./db')
 const Station = require("./model");
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/:address', async(req, res) => {
+    let { address } = req.params;
+    const coordinates = await GetCoordinates(address);
+    connectDB();
+    await Station.find(
+        {
+            location:{
+                $near: {
+                    $geometry:{
+                        type: "Point",
+                        coordinates 
+                    },
+                    $maxDistance: 20*20
+                } 
+            }
+        }
+    ).exec((err, location)=>{
+        if(err) {
+            return  res.status(500).send({
+                status: flase,
+                data: err
+            })
+        }
+        if(location){
+            return  res.status(200).send({
+                status: true,
+                data: location
+            })
+        }
+    })
+})
+//refer from Roy
 async function GetCoordinates(address) {
     let data = await (await axios.get(`https://www.google.com/maps/place?q=${encodeURI(address)}`)).data;
     data = data.toString();
@@ -14,32 +51,6 @@ async function GetCoordinates(address) {
     const lng = data.slice(data.indexOf('%2C') + 3, data.indexOf('&amp'));
     return [parseFloat(lng), parseFloat(lat)];
 }
-
-async function GetNearSpot(address){
-    console.log("address: ", address);
-    const coordinates = await GetCoordinates(address);
-    console.log("coordinates: ", coordinates);
-    connectDB();
-    const res = Station.find(
-        {
-        location:{
-            $near: {
-                $geometry:{
-                    type: "Point",
-                    coordinates 
-                },
-                $maxDistance: 20*20
-            } 
-        }
-    }
-    ).exec((err, location)=>{
-        if(err) console.log(err);
-        else{
-            console.log("location: ", location);
-        }
-    })
-}
-
-setInterval(async()=>{
-    await GetNearSpot("台北101");
-},3000);
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
